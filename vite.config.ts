b9889bdbd425e38d79a4d969f5a8ceb9d9e3d1eb
Omotola Oyeniyi <kingsx@localhost.localdomain>
2025-05-18 18:47:03 +0100

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/thollarkings-portfolio1/",
+  base: "",
   server: {
     host: "::",
     port: 8080,
